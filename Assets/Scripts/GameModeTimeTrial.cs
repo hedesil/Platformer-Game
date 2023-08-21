1ee25740b834e1b8917b6fef967450eba3bd62a8
Hedesil <mariano25294@gmail.com>
2023-08-21 20:37:47 +0200

    Added game trial mode

---
+++ Assets/Scripts/GameModeTimeTrial.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameModeTimeTrial : MonoBehaviour
+{
+    public GameManager gameManager;
+    public float levelTimer = 300f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(levelTimer > 0) {
+            levelTimer = levelTimer - Time.deltaTime;
+        } else {
+            if(gameManager.isGameOver == false) {
+                gameManager.isGameOver = true;
+                gameManager.Die();
+            }
+        }
+    }
+}
