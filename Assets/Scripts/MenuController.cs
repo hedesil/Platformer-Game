8deb3824b378ec021e3d76035d6fec0483089899
Hedesil <mariano25294@gmail.com>
2023-08-22 20:23:54 +0200

    Added menu controller

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void LoadGameLevel(int levelId)
+    {
+        SceneManager.LoadScene(levelId);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
